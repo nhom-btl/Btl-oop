c96f7f9df214fa1a6faa04159af11852d4156bc4
HoangHaJu <130366982+HoangHaJu@users.noreply.github.com>
2023-05-05 21:30:11 +0700

    Add files via upload

---
+++ test/DINO/ObstaclesGroupTest.java
@@ -0,0 +1,53 @@
+package DINO;
+
+import static org.junit.Assert.*;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Vector;
+import javax.imageio.ImageIO;
+import org.junit.Before;
+import org.junit.Test;
+import pkg2dgamesframework.Objects;
+import pkg2dgamesframework.QueueList;
+import java.util.Queue;
+import java.util.Scanner; 
+
+
+public class ObstaclesGroupTest {
+  @Test
+    public void testObstaclesGroup() {
+        ObstaclesGroup obstaclesGroup = new ObstaclesGroup();
+        QueueList<ObstaclesGroup.Obstacles> x = obstaclesGroup.getXrs();
+        BufferedImage[] arr = obstaclesGroup.getImgXr();
+        assertEquals(7, obstaclesGroup.SIZE);
+        assertEquals(780, obstaclesGroup.posvatcanfirst);
+         assertEquals(500, obstaclesGroup.khoangcach2vatcan);
+        assertNotNull(x);
+        assertNotNull(arr);
+        assertNotNull(obstaclesGroup.rdimg);
+        assertNotNull(obstaclesGroup.ktanh);
+        
+    }
+    
+    @Test
+    public void testUpdateDiamondGroup() {
+        DiamondGroup diamondGroup= new DiamondGroup();
+        float initialPosX = diamondGroup.getdia(0).getPosX();
+        diamondGroup.update();
+        float updatedPosX = diamondGroup.getdia(0).getPosX();
+        assertTrue(updatedPosX < initialPosX);
+           
+    }
+@Test
+    public void testPaintDiamondGroup() {
+        DiamondGroup diamondGroup= new DiamondGroup();
+        float initialPosX = diamondGroup.getdia(0).getPosX();
+        diamondGroup.update();
+        float updatedPosX = diamondGroup.getdia(0).getPosX();
+        assertTrue(updatedPosX < initialPosX);
+           
+    }
+}
