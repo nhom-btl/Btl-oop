bf4494ebfdfdae07761ef1ec0bcfbd593622584a
hoanganhhh-bb <64246173+hoanganhhh-bb@users.noreply.github.com>
2023-05-07 15:55:22 +0700

    Update Sky.java

--- src/DINO/Sky.java
+++ src/DINO/Sky.java
@@ -4,16 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Sky extends Background{  
+public class Sky extends Background{  //sky mr tư bgr
     public Sky() {
         try {
             this.imgbgr = ImageIO.read(new File("images/night.png"));
+            //đọc hình ảnh cho hình nên game từ tập tin night. trong thư mục image
+            //bằng cách sd imageio 
         }   catch (IOException ex){}   
-        
-        //y1 = 0;    
-        //x1 = 0;
-        
+        //neu k tìm thấy tệp tin hoặc có lỗi xảy ra trong quá trình đọc tệp tin, 1 ngoại lên exception sẽ được ném và bị bỏ qua
+        //hình ảnh này sau đó được lưu trong thuộc tính imbgr của đối tượng sky
+        y1 = 0;    
+        x1 = 0;
+        //toa do góc bên trái của hình nền (0,0)
         x2 = x1 + 780;
         y2 = y1;
+        //tọa độ gốc dưới phải của màn hình(780,0)
+        // tọa độ được sử dụng để vẽ hình nền trong trò chơi
     }  
 }
