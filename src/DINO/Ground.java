3cbed3e90b9de88a932346ddf8a6fa8be6b12463
hoanganhhh-bb <64246173+hoanganhhh-bb@users.noreply.github.com>
2023-05-07 15:50:04 +0700

    Update Ground.java

--- src/DINO/Ground.java
+++ src/DINO/Ground.java
@@ -4,15 +4,15 @@ package DINO;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-
+//import cac thu vien can thiet
 public class Ground extends Background{
 
     public Ground() {
         try {
-            this.imgbgr = ImageIO.read(new File("images/nen.png"));
+            this.imgbgr = ImageIO.read(new File("images/nen.png")); // them anh vao
         }   catch (IOException ex){}
         
-        x1 = 0;
+        x1 = 0; // co the co , tu khoi tao
         y1 = 400;
         x2 = x1 + 780;
         y2 = y1;
