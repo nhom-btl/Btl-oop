687623a08672aa60164cc8da92ace3db1e0ca363
hoanganhhh-bb <64246173+hoanganhhh-bb@users.noreply.github.com>
2023-05-07 15:47:56 +0700

    Update Sky.java

--- src/DINO/Sky.java
+++ src/DINO/Sky.java
@@ -8,9 +8,11 @@ public class Sky extends Background{
     public Sky() {
         try {
             this.imgbgr = ImageIO.read(new File("images/night.png"));
-        }   catch (IOException ex){}        
-        x1 = 0;
-        y1 = 0;
+        }   catch (IOException ex){}   
+        
+        //y1 = 0;    
+        //x1 = 0;
+        
         x2 = x1 + 780;
         y2 = y1;
     }  
